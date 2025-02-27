4421ffebfa401d6ef17d315adb0bbedb424e7635
Matt Ellis <m.t.ellis@gmail.com>
2025-02-27 13:24:04 +0000

    Add iterm2 integration files

--- links.sh
+++ links.sh
@@ -4,3 +4,5 @@ ln -s dotfiles/gitconfig ~/.gitconfig
 ln -s dotfiles/hgrc ~/.hgrc
 ln -s dotfiles/ideavimrc ~/.ideavimrc
 ln -s dotfiles/inputrc ~/.inputrc
+ln -s dotfiles/iterm2_shell_integration.zsh ~/.iterm2_shell_integration.zsh
+ln -s dotfiles/iterm2_shell_integration.bash ~/.iterm2_shell_integration.bash
